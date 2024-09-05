ea15f3c72cab6446934e55acdb3c7e8e568bd83a
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-05 12:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,33 @@
-// const makeChange = (c) => {
-//   your name here
-// 	let obj = {
-// 		q : 0,
-// 		d : 0,
-// 		n : 0,
-// 		p : 0
-// 	}
-// 	if (c >= 25) {
-// 		obj.q = Math.floor(c / 25);
-// 		c = c % 25;
-// 	}
-// 	 if (c >= 10 ) {
-// 		obj.d = Math.floor(c / 10);
-// 		c = c % 10;
-// 	}
-// 	 if (c >= 5 ) {
-// 		obj.n = Math.floor(c / 5);
-// 		c = c % 5;
-// 	} if (c >= 1) {
-// 		obj.p =  c ;
-// 	}
-// 	console.log(obj);
-// 	return obj;
-// };
-
 const makeChange = (c) => {
-  
-  c = Number(c);
-  
-  let change = {
-    q: 0, 
-    d: 0, 
-    n: 0, 
-    p: 0  
-  };
+  your name here
+
+	c = Number(c);
+	
+	let obj = {
+		q : 0,
+		d : 0,
+		n : 0,
+		p : 0
+	}
+	if (c >= 25) {
+		obj.q = Math.floor(c / 25);
+		c = c % 25;
+	}
+	 if (c >= 10 ) {
+		obj.d = Math.floor(c / 10);
+		c = c % 10;
+	}
+	 if (c >= 5 ) {
+		obj.n = Math.floor(c / 5);
+		c = c % 5;
+	} if (c >= 1) {
+		obj.p =  c ;
+	}
+	
+	return obj;
+};
 
 
-  change.q = Math.floor(c / 25);
-  c %= 25;
-
-
-  change.d = Math.floor(c / 10);
-  c %= 10;
-
-
-  change.n = Math.floor(c / 5);
-  c %= 5;
-
-
-  change.p = c;
-
-  return change;
 };
 // Do not the change the code below
 const c = prompt("Enter c: ");
